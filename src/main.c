015a89d177ef31156c76020a02b4d1c48edb86b8
Denis <denis_kulikov_a_2003@mail.ru>
2023-05-21 01:40:56 +0700

    Runge-Kutta method for first-order ODEs

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+const double h = 0.1;
+
 struct data
 {
     int a, b;
@@ -8,10 +10,27 @@ struct data
     double *value;
 };
 
+double fun (double x, double y)
+{
+    return x + y;
+}
+
+double Runge_Kutta (double x, double y)
+{
+    double k1, k2, k3, k4;
+
+    k1 = h * fun (x, y);
+    k2 = h * fun (x + h / 2, y + k1 / 2);
+    k3 = h * fun (x + h / 2, y + k2 / 2);
+    k4 = h * fun (x + h, y + k3);
+
+    return y + (k1 + 2 * k2 + 2 * k3 + k4) / 6;
+}
+
 
 int main (int argc, char* argv[])
 {
-    printf("%s\n", argv[0]);
+    printf("%f %f\n", Runge_Kutta(0, 1), Runge_Kutta(h, Runge_Kutta(0, 1)));
 
     return 0;
 }
